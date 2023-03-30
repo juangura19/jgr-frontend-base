88076e510c31a46e2841453243738ed20fc1ec63
Juan GUTIÉRREZ RAMOS <juangura19@gmail.com>
2023-03-30 14:51:47 -0500

    new version angular 15

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatPaginatorIntl } from '@angular/material/paginator';
+import { MatLegacyPaginatorIntl as MatPaginatorIntl } from '@angular/material/legacy-paginator';
 import { PaginatorI18n } from './utils/adapters/paginator.adapter';
 import { SnackBackAdapter } from "./utils/adapters/snackBar.adapter";
 
